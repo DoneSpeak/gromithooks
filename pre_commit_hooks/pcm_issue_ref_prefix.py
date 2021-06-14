cd1b6da648b8e6e70443aa758ad42f2b6970973d
Donespeak <guanrong.yang@foxmail.com>
2021-06-14 21:04:01 +0800

    add stage to pcm_issue_ref_prefix config

--- pre_commit_hooks/pcm_issue_ref_prefix.py
+++ pre_commit_hooks/pcm_issue_ref_prefix.py
@@ -1,8 +1,6 @@
 import sys, os
 import argparse
 
-sys.argv[1]
-
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to fix')
